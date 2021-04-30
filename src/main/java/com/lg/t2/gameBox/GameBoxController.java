6f6110dad2d068c8f7287abc5788043f722fe6f0
hancheol park <hancheol-park>
2021-04-30 18:11:57 +0900

    성현씨

--- src/main/java/com/lg/t2/gameBox/GameBoxController.java
+++ src/main/java/com/lg/t2/gameBox/GameBoxController.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.tomcat.util.log.UserDataHelper.Mode;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
